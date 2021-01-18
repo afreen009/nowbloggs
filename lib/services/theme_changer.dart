322ea8b7b1b76aa3123a7681dbf98555a378a4db
afreen009 <shfafreen@gmail.com>
2021-01-18 19:46:12 +0530

    second

---
+++ lib/services/theme_changer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ThemeChanger with ChangeNotifier {
+  var _themeMode = ThemeMode.light;
+
+  get getTheme => _themeMode;
+
+  toggle() {
+    var _newTheme =
+        (_themeMode == ThemeMode.light) ? ThemeMode.dark : ThemeMode.light;
+    setTheme(_newTheme);
+  }
+
+  setTheme(themeMode) {
+    _themeMode = themeMode;
+    notifyListeners();
+  }
+}
